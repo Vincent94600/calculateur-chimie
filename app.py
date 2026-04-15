593f79310427abbdb0e2943016c6408a16e599e3
Vincent94600 <vincentfaragout@gmail.com>
2026-04-15 15:30:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if choix == "Solide":
         m_mol = st.number_input("Masse molaire du solide (g/mol)", min_value=0.0, value=58.44)
         masse = c * v * m_mol
     
-    st.metric("Masse à peser", f"{masse:.4f} g")
+    st.metric("Masse à peser :", f"{masse:.4f} g")
 
 # --- 2. MODULE LIQUIDES ---
 elif choix == "Liquide":
@@ -48,7 +48,7 @@ elif choix == "Liquide":
         v_prelev = (target_c * v_sol * m_mol_l) / (purete * d)
         m_prelev = (target_c * v_sol * m_mol_l) / (purete)
     
-    st.metric("Volume à prélever" , "Masse à prélever" , f"{v_prelev:.2f} mL", f"{m_prelev:.2f} g")    
+    st.metric("Volume à prélever : OU Masse à prélever :" , f"{v_prelev:.2f} mL OU {m_prelev:.2f} g")    
     st.metric("Masse à prélever", f"{m_prelev:.2f} g")
 
 # --- 3. MODULE DILUTION ---
@@ -64,5 +64,5 @@ elif choix == "Dilution (Mère/Fille)":
         st.error("La fille doit être moins concentrée que la mère !")
     else:
         v_mere = (c_fille * v_fille) / c_mere
-        st.metric("Volume MÈRE à prélever", f"{v_mere:.2f} mL")
+        st.metric("Volume MÈRE à prélever :", f"{v_mere:.2f} mL")
         st.write(f"👉 Verser {v_mere:.2f} mL de mère et compléter jusqu'à {v_fille} mL.")
