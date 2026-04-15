0de402e54e936a3d15ef3480f92715dfe571f27e
Vincent94600 <vincentfaragout@gmail.com>
2026-04-15 15:42:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ elif choix == "Liquide":
         m_prelev = (target_c * v_sol * m_mol_l) / (purete)
     
     st.metric("Volume à prélever :" , f"{v_prelev:.2f} mL")
-    st.write("#OU")
+    st.write("<h1> OU </h1>",unsafe_allow_html=True)
     st.metric("Masse à prélever", f"{m_prelev:.2f} g")
 
 # --- 3. MODULE DILUTION ---
